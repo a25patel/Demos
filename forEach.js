18e7c0810b1006d3225e9eddb0867477cf8790e7
a25patel <aanaletap2510@gmail.com>
2015-07-29 10:19:25 -0600

    forEach drills complete

--- forEach.js
+++ forEach.js
@@ -1,6 +1,6 @@
 // 1. iterate over an array of numbers, log the number
 
-var array = [1,3,5,8,9,6,4,2,4,6,7].forEach(function(num){
+var array = [1,3,5,8,9,6].forEach(function(num){
   console.log(num);
 })
 
@@ -17,10 +17,17 @@ console.log(result);
 // 3. ['a', 'b', 'c'] => 'a,b,c' !No comma after c!
 
 var result ='';
-['a', 'b', 'c'].forEach(function(letter, i , array){
+['apples', 'bananas', 'cantaloupe'].forEach(function(letter, i , array){
   if((array.length-1) === i ){
     result += letter
   }else{
     result += letter + ','
   }
+})
+
+// 4. [{name: 'Tom', age: '34'}, {name: 'Betty', age:'21'}] => Tom 34 , Better 37
+
+var array = [{name: 'Tom', age: '34'}, {name: 'Betty', age:'21'}];
+array.forEach(function(object){
+  console.log(object.name + ': ' + object.age);
 })
